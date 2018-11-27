6e0c6e1ad094916874579ec97b021d82ff90d8dc
SyaoKyo <chengs.love@163.com>
2018-11-27 14:14:08 +0800

    树

--- deepFirstTraversal/main.cpp
+++ deepFirstTraversal/main.cpp
@@ -1,10 +1,22 @@
-/************************
- *                      *
- *  二叉树深度优先遍历  *
- *                      *
- ************************/
+/******************
+ *                *
+ *  深度优先遍历  *
+ *                *
+ ******************/
 #include <iostream>
 using namespace std;
+
+struct Branch
+{
+	int cIdx;
+	Branch *next;
+};
+
+struct TNode
+{
+	int data;
+	Branch *first;
+};
 
 struct BTNode
 {
@@ -12,12 +24,17 @@ struct BTNode
 	BTNode *lChild;
 	BTNode *rChild;
 };
-void testTree(BTNode *root);
+
+void testBTree(BTNode *root);
+void testTree(TNode t[]);
 void visit(BTNode *p);//访问结点
+void visit(TNode *p);//访问结点
 void r(BTNode *p);//深度优先遍历（递归）
 void preorderNoneRecursion(BTNode *bt);//先序深度优先遍历（非递归）
 void inorderNoneRecursion(BTNode *bt);//中序深度优先遍历（非递归）
 void postorderNoneRecursion(BTNode *bt);//后序深度优先遍历（非递归）
+void preOrder(TNode *p, TNode tree[]);//树的先序深度优先遍历
+void postOrder(TNode *p, TNode tree[]);//树的后序深度优先遍历
 
 const int maxSize = 1000;
 int arr[maxSize];
@@ -25,16 +42,21 @@ int arri = 0;
 
 int main()
 {
-	BTNode *root=new BTNode();
-	testTree(root);
-	r(root);//递归
+	BTNode root;
+	testBTree(&root);
+	cout << "BTree:" << endl;
+	r(&root);//递归
 	cout << endl;
-	preorderNoneRecursion(root);//非递归
+	preorderNoneRecursion(&root);//非递归
 	cout << endl;
+	cout << "Tree:" << endl;
+	TNode tree[maxSize];
+	testTree(tree);
+	preOrder(tree, tree);
 	return 0;
 }
 
-void testTree(BTNode *root)
+void testBTree(BTNode *root)
 {
 	root->data = 1;
 	root->lChild = NULL;
@@ -100,8 +122,59 @@ void testTree(BTNode *root)
 	newp12->rChild = NULL;
 	newp6->rChild = newp12;
 }
+void testTree(TNode t[])
+{
+	for (int i = 0;i < 10;i++)
+	{
+		t[i].data = i + 1;
+		t[i].first = NULL;
+	}
+	Branch *newB2 = new Branch();
+	newB2->cIdx = 1;
+	newB2->next = NULL;
+	t[0].first = newB2;
+	Branch *newB3 = new Branch();
+	newB3->cIdx = 2;
+	newB3->next = NULL;
+	newB2->next = newB3;
+	Branch *newB4 = new Branch();
+	newB4->cIdx = 3;
+	newB4->next = NULL;
+	newB3->next = newB4;
+	Branch *newB5 = new Branch();
+	newB5->cIdx = 4;
+	newB5->next = NULL;
+	t[1].first = newB5;
+	Branch *newB6 = new Branch();
+	newB6->cIdx = 5;
+	newB6->next = NULL;
+	t[2].first = newB6;
+	Branch *newB7 = new Branch();
+	newB7->cIdx = 6;
+	newB7->next = NULL;
+	newB6->next = newB7;
+	Branch *newB8 = new Branch();
+	newB8->cIdx = 7;
+	newB8->next = NULL;
+	newB7->next = newB8;
+	Branch *newB9 = new Branch();
+	newB9->cIdx = 8;
+	newB9->next = NULL;
+	t[3].first = newB9;
+	Branch *newB10 = new Branch();
+	newB10->cIdx = 9;
+	newB10->next = NULL;
+	newB9->next = newB10;
+}
 
 void visit(BTNode *p)
+{
+	//可打印
+	cout << p->data << " -> ";
+	//可存入数组
+	//arr[arri++] = p->data;
+}
+void visit(TNode *p)
 {
 	//可打印
 	cout << p->data << " -> ";
@@ -191,5 +264,35 @@ void postorderNoneRecursion(BTNode *bt)
 			p = Stack2[top2--];
 			visit(p);
 		}
+	}
+}
+
+void preOrder(TNode *p, TNode tree[])
+{
+	if (p != NULL)
+	{
+		visit(p);
+		Branch *q;
+		q = p->first;
+		while (q != NULL)
+		{
+			preOrder(&tree[q->cIdx], tree);
+			q = q->next;
+		}
+	}
+}
+
+void postOrder(TNode *p, TNode tree[])
+{
+	if (p != NULL)
+	{
+		Branch *q;
+		q = p->first;
+		while (q != NULL)
+		{
+			preOrder(&tree[q->cIdx], tree);
+			q = q->next;
+		}
+		visit(p);
 	}
 }
